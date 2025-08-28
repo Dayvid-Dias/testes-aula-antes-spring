db0b80e17ccf9edb52ff40a97a04e43eaa58eafc
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-27 22:32:39 -0300

    test main

--- src/SistemaPagamentos/main.java
+++ src/SistemaPagamentos/main.java
@@ -3,10 +3,9 @@ package SistemaPagamentos;
 public class main {
     public static void main(String[] args) {
 
-        Cliente cliente = new Cliente("dayvidolas", 1000);
+        Cliente cliente = new Cliente("Jubiscleudo", 5000);
 
-        cliente.realizarPagamento(450, new Pix());
-        cliente.realizarPagamento(250, new Boleto());
+        cliente.realizarPagamento(3500, new Boleto());
 
     }
 }
