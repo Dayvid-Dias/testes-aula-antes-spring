ee2c886dbd867590b6bc6ba69a20e1b891b3c088
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-27 23:34:20 -0300

    add class Estagiario

---
+++ src/Funcionarios/Estagiario.java
@@ -0,0 +1,21 @@
+package Funcionarios;
+
+public class Estagiario extends Funcionario implements Trabalhavel{
+    double bonus;
+
+    public Estagiario(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    double calcularBonus() {
+        bonus = salario * 0.05;
+        System.out.println("Salário com bônus: R$ " + (salario + bonus));
+        return bonus;
+    }
+
+    @Override
+    public void trabalhar() {
+        System.out.println(nome + " está trabalhando!");
+    }
+}
