f63f1ea2a63af9769df01a13d08d80533ce5f53d
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-29 11:46:36 -0300

    add new class ex

--- src/ListSetEx/NomesUnicos.java
+++ src/ListSetEx/NomesUnicos.java
@@ -1,7 +1,29 @@
 package ListSetEx;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NomesUnicos {
     public static void main(String[] args) {
-        
+        Set<String> nomes = new HashSet<>();
+
+        nomes.add("Ana");
+        nomes.add("Pedro");
+        nomes.add("Maria");
+        nomes.add("Ana");
+        nomes.add("Pedro");
+
+        for (String nome : nomes){
+            System.out.println(nome);
+        }
+        if (nomes.contains("João")){
+            System.out.println("João está na lista.");
+        }else{
+            System.out.println("João não está na lista.");
+        }
+        nomes.remove("Pedro");
+        System.out.println(nomes);
+        System.out.println("Tamanho da lista: " + nomes.size());
+
     }
 }
