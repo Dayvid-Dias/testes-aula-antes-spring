16b38fe0685dd8d155e6668b4f54323f422d99b5
Dayvid Dias <dayvid.dias23@gmail.com>
2025-08-29 10:58:36 -0300

    add logic of ListaDeCompras

--- src/ListSetEx/ListaDeCompras.java
+++ src/ListSetEx/ListaDeCompras.java
@@ -1,4 +1,23 @@
 package ListSetEx;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaDeCompras {
+    public static void main(String[] args) {
+        List<String> listaCompras = new ArrayList<>();
+
+        listaCompras.add("carne");
+        listaCompras.add("morango");
+        listaCompras.add("ovo");
+        listaCompras.add("carne");
+        listaCompras.add("atum");
+
+        for (String compras : listaCompras){
+            System.out.println(compras);
+        }
+        System.out.println("Tamanho da lista: " + listaCompras.size());
+        System.out.println("item do índice 2: " + listaCompras.get(2));
+        listaCompras.remove("morango");
+    }
 }
